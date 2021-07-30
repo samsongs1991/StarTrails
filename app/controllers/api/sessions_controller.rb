f782fc1ec092e7b24a4974b243f7cc18f1cf8d4d
Samuel Song <samsongs@umich.edu>
2021-07-30 17:31:24 -0600

    Add session controller actions CREATE and DESTROY

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,34 @@
 class Api::SessionsController < ApplicationController
 
     def create
+        @user = User.find_by_credentials(
+            params[:user][:email], 
+            params[:user][:password]
+        )
+
+        if @user 
+            login(@user) 
+            render "/api/users/home"
+        else
+            render json: { signIn: ["Invalid email or password"] }, status: 401
+        end
     end
 
     def destroy
+        @user = current_user
+
+        if @user 
+            # if there's a user logged in
+            # log out the current user
+            logout 
+        end
+
+        # after clicking the logout button
+        # reroute to home page regardless
+        # only 2 cases
+        #  - current_user exists => log out and go to home page
+        #  - current_user doesn't exist => do nothing and go to home page
+        render "/api/users/home"
     end
     
 end
