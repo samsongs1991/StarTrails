a4ce3749d0e50c4a518cbe8b3731b2145b384b97
Samuel Song <samsongs@umich.edu>
2021-07-30 17:09:35 -0600

    Add users controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,25 @@
+class Api::UsersController < ApplicationController
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            # after signing up, login
+            # then reroute to home page "/"
+            # except with personal greeting
+            login(@user)
+            render "/"
+        else
+            render json: { signUp: ["Invalid email or password"] }, status: 422
+        end
+    end
+
+    # -------------------------------------------------------- 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+    
+end
