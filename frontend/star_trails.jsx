38bf8e721e9120f8209ef2b6f5a58789e8f50f23
Samuel Song <samsongs@umich.edu>
2021-07-31 11:34:46 -0600

    Add root component to render in entry file

--- frontend/star_trails.jsx
+++ frontend/star_trails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Root from "./root"
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<h1>React is working!</h1>, document.getElementById("root"));
+  ReactDOM.render(<Root />, document.getElementById("root"));
 });
