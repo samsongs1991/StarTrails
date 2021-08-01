5651f8e16d105a3ddfc694877b83a6072616bc9b
Samuel Song <samsongs@umich.edu>
2021-08-01 11:18:22 -0600

    Add entities reducer combined with users reducer and session reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,12 +1,12 @@
 import { RECEIVE_CURRENT_USER, SIGNOUT_CURRENT_USER } from "../actions/session_actions";
 
-const _nullUser = { id: null }
+const _nullUser = { currentUserId: null }
 
 const SessionReducer = (oldState=_nullUser, action) => {
     Object.freeze(oldState);
     switch(action.type) {
         case RECEIVE_CURRENT_USER:
-            return Object.assign({}, oldState, { id: action.currentUser.id });
+            return Object.assign({}, oldState, { currentUserId: action.currentUser.id });
         case SIGNOUT_CURRENT_USER: 
             return _nullUser;
         default:
