fa93970a105c59666a8c22a5b98197409997fa0a
Samuel Song <samsongs@umich.edu>
2021-08-01 12:54:32 -0600

    Add greeting container component

---
+++ frontend/components/greeting/greeting_container.js
@@ -0,0 +1,13 @@
+import { connect } from "react-redux";
+import Greeting from "./greeting";
+import { signout } from "../../actions/session_actions";
+
+const mapStateToProps = ({ entities: { users }, session: { currentUserId } }) => ({
+    currentUser: users[currentUserId]
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    signout: () => dispatch(signout())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Greeting);
