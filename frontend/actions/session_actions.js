37433b14a21374421f25630f7db68ed942828199
Samuel Song <samsongs@umich.edu>
2021-08-01 11:12:29 -0600

    Add users reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,5 +1,7 @@
 import * as SessionApiUtils from "../util/session_api_util";
 
+// -------------------------------------------------
+
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 const receiveCurrentUser = (currentUser) => ({
     type: RECEIVE_CURRENT_USER, 
