3d510745b19959515ec4a1148d61f9626571ff05
Samuel Song <samsongs@umich.edu>
2021-08-01 11:37:02 -0600

    Add root reducer and combined entities reducer, session reducer, errors reducer. Remove session reducer from entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import UsersReducer from "./users_reducer";
-import SessionReducer from "./session_reducer";
 
 const EntitiesReducer = combineReducers({
     users: UsersReducer, 
-    session: SessionReducer
 });
 
 export default EntitiesReducer;
