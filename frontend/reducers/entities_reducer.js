8855303d0a4ecb85356e499db6457cd59b50d9a6
Samuel Song <samsongs@umich.edu>
2021-08-03 15:55:32 -0600

    Add trails reducer to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import UsersReducer from "./users_reducer";
+import TrailsReducer from "./trails_reducer";
 
 const EntitiesReducer = combineReducers({
     users: UsersReducer, 
+    trails: TrailsReducer
 });
 
 export default EntitiesReducer;
