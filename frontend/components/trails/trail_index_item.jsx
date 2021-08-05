3fa862041cfc33057b7ae6a1ac79cfd39a304fa2
Samuel Song <samsongs@umich.edu>
2021-08-05 13:16:10 -0600

    Add trail show util functions for trail index item

--- frontend/components/trails/trail_index_item.jsx
+++ frontend/components/trails/trail_index_item.jsx
@@ -48,21 +48,19 @@ class TrailIndexItem extends React.Component {
         return (
             <li>
                 <div>
-                    <img src="" alt="" />
-                    <h3>trail.name</h3>
-                    <div>{trailLocation(trail)}</div>
+                    <img src="" alt={"Image of " + trail.name} />
+                    <h3>{trail.name}</h3>
+                    <div>{this.trailLocation(trail)}</div>
                     <div>
                         {TrailShowUtil.difficultyLabel(trail)}
-                        {/* <DifficultyLabel /> */}
-                        {TrailShowUtil.starRating(trail)}
-                        {/* <StarRating /> */}
-                        {TrailShowUtil.numReviews(trail)}
-                        {/* # of reviews */}
+                        {/* --- BELOW --- uncomment after reviews portion of DB is built */}
+                        {/* {TrailShowUtil.starRating(trail)}
+                        {TrailShowUtil.numReviews(trail)} */}
                     </div>
                     <div>
                         <p>Length: {trail.length} mi</p>
                         <p> - </p>
-                        <p>Est. {trailTime(trail)}</p>
+                        <p>Est. {this.trailTime(trail)}</p>
                     </div>
                 </div>
 
--- frontend/util/trail_show_util.jsx
+++ frontend/util/trail_show_util.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
 export const difficultyLabel = trail => {
-
+    if(trail.difficulty === "easy") {
+        return <img src="images/EasyLabel.png" alt="Easy" />;
+    } else if (trail.difficulty === "moderate") {
+        return <img src="images/ModerateLabel.png" alt="Moderate" />;
+    } else if (trail.difficulty === "hard") {
+        return <img src="images/HardLabel.png" alt="Hard" />;
+    }
 }
 
-export const starRating = trail => {
+// --- BELOW --- will complete after building out the reviews portion of DB 
 
-}
+// export const starRating = trail => {
 
-export const numReviews = trail => {
+// }
 
-}
+// export const numReviews = trail => {
+
+// }
