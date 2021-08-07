45262baebbe1a44ef4956d05f241f95c3bd4e7fa
Samuel Song <samsongs@umich.edu>
2021-08-07 17:02:42 -0600

    Edit users reducer to account for new user actions

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -3,6 +3,10 @@ import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 const UsersReducer = (oldState={}, action) => {
     Object.freeze(oldState);
     switch(action.type) {
+        case RECEIVE_USERS:
+            return action.users
+        case RECEIVE_USER:
+            return Object.assign({}, oldState, { [action.user.id]: action.user });
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, oldState, { [action.currentUser.id]: action.currentUser }); 
         default:
