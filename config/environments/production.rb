f55a6534bc7ed3984f1da3447eb09f6c047370dc
Samuel Song <samsongs@umich.edu>
2021-08-07 15:14:05 -0600

    Add AWS services to config environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+
+  # AWS ------------------------------------------------------------
+  config.active_storage.service = :amazon_prod
+  
+
+  
+  
+  
 end
