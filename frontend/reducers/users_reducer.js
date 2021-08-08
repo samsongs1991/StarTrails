7db3b7a899f3432ff19cdeb1587bcdee5d720d5b
Samuel Song <samsongs@umich.edu>
2021-08-07 19:24:50 -0600

    Fix import bug in users reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,10 +1,10 @@
-import { RECEIVE_USERS, RECEIVE_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
+import { RECEIVE_USERS, RECEIVE_USER, RECEIVE_CURRENT_USER } from "../actions/user_actions";
 
 const UsersReducer = (oldState={}, action) => {
     Object.freeze(oldState);
     switch(action.type) {
         case RECEIVE_USERS:
-            return action.users
+            return Object.assign({}, oldState, action.users);
         case RECEIVE_USER:
             return Object.assign({}, oldState, { [action.user.id]: action.user });
         case RECEIVE_CURRENT_USER:
