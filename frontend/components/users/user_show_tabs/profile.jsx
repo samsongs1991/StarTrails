2d05956c152c48a382e35f9cb288c8eda5463ce2
Samuel Song <samsongs@umich.edu>
2021-08-09 13:03:21 -0600

    Add detailed skeleton for profile component

--- frontend/components/users/user_show_tabs/profile.jsx
+++ frontend/components/users/user_show_tabs/profile.jsx
@@ -3,7 +3,27 @@ import React from "react";
 class Profile extends React.Component {
     render() {
         return (
-            <div>Profile component works</div>
+            <div>
+                {/* <div>Profile component works</div> */}
+                <div>
+                    <h3>Profile</h3>
+                    <div>
+                        <div>
+                            {/* Profile pic */}
+                            {/* Member Since */}
+                        </div>
+
+                        <div>
+                            {/* Name */}
+                            {/* City, State */}
+                        </div>
+                        
+                        <div>
+                            {/* Recent Activity */}
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
