40fa8300ad8581b83c0e36b8ea530a9f18a4e552
Samuel Song <samsongs@umich.edu>
2021-08-09 15:45:31 -0600

    Edit entities reducer to include userDetails

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import UsersReducer from "./users_reducer";
 import TrailsReducer from "./trails_reducer";
+import UserDetailsReducer from "./user_details_reducer";
 
 const EntitiesReducer = combineReducers({
     users: UsersReducer, 
+    userDetails: UserDetailsReducer,
     trails: TrailsReducer
 });
 
