26241487ce945cc46e2f2330a7bf2437dd0b2d66
Samuel Song <samsongs@umich.edu>
2021-08-09 15:51:28 -0600

    Fix bug in user details reducer for missing import reference

--- frontend/reducers/user_details_reducer.js
+++ frontend/reducers/user_details_reducer.js
@@ -3,7 +3,7 @@ import { RECEIVE_USER_DETAIL } from "../actions/user_detail_actions";
 const UserDetailsReducer = (oldState={}, action) => {
     Object.freeze(oldState);
     switch(action.type) {
-        case RECEIVE_USER_DETAILS:
+        // case RECEIVE_USER_DETAILS:
             // when receiving many details
         case RECEIVE_USER_DETAIL:
             return Object.assign({}, oldState, { [action.userDetail.userId]: action.userDetail }); 
