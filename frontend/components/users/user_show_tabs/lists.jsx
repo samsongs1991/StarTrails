bf338a4c9fe435b70feb51bdd197fc689752f88b
Samuel Song <samsongs@umich.edu>
2021-08-10 11:24:50 -0600

    Add details to lists tab component

--- frontend/components/users/user_show_tabs/lists.jsx
+++ frontend/components/users/user_show_tabs/lists.jsx
@@ -3,7 +3,22 @@ import React from "react";
 class Lists extends React.Component {
     render() {
         return (
-            <div>Lists component works</div>
+            <div>
+                <div>
+                    <h3>Lists</h3>
+                    <button>Create a list</button>
+                    {/* Create a onClick handler for button */}
+                </div>
+
+                <div>
+                    <ul>
+                        <li>List item component 1</li>
+                        <li>List item component 2</li>
+                        <li>List item component 3</li>
+                        {/* Create list item components */}
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
