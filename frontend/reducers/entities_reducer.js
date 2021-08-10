900afca044ef3acf6a06b5b573a759f4c2c5a35d
Samuel Song <samsongs@umich.edu>
2021-08-09 18:19:15 -0600

    Remove user details reducer from entities reducer imports

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 import UsersReducer from "./users_reducer";
 import TrailsReducer from "./trails_reducer";
-import UserDetailsReducer from "./user_details_reducer";
 
 const EntitiesReducer = combineReducers({
     users: UsersReducer, 
-    userDetails: UserDetailsReducer,
     trails: TrailsReducer
 });
 
