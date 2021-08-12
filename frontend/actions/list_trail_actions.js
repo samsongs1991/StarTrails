37a96ee23dbb91c475f7cb287b9b5e1dbe009366
Samuel Song <samsongs@umich.edu>
2021-08-11 22:30:18 -0600

    Uncomment lists trails code in frontend folder

--- frontend/actions/list_trail_actions.js
+++ frontend/actions/list_trail_actions.js
@@ -1,41 +1,41 @@
-// import * as ListTrailApiUtils from "../util/list_trail_api_util";
+import * as ListTrailApiUtils from "../util/list_trail_api_util";
 
-// export const RECEIVE_LIST_TRAIL_RELATIONS = "RECEIVE_LIST_TRAIL_RELATIONS";
-// const receiveListTrailRelations = listTrailRelations => ({
-//     type: RECEIVE_LIST_TRAIL_RELATIONS, 
-//     listTrailRelations
-// })
+export const RECEIVE_LIST_TRAIL_RELATIONS = "RECEIVE_LIST_TRAIL_RELATIONS";
+const receiveListTrailRelations = listTrailRelations => ({
+    type: RECEIVE_LIST_TRAIL_RELATIONS, 
+    listTrailRelations
+})
 
-// export const RECEIVE_LIST_TRAIL_RELATION = "RECEIVE_LIST_TRAIL_RELATION";
-// export const receiveListTrailRelation = listTrailRelation => ({
-//     type: RECEIVE_LIST_TRAIL_RELATION, 
-//     listTrailRelation
-// })
+export const RECEIVE_LIST_TRAIL_RELATION = "RECEIVE_LIST_TRAIL_RELATION";
+export const receiveListTrailRelation = listTrailRelation => ({
+    type: RECEIVE_LIST_TRAIL_RELATION, 
+    listTrailRelation
+})
 
-// export const REMOVE_LIST_TRAIL_RELATION = "REMOVE_LIST_TRAIL_RELATION";
-// const removeListTrailRelation = listTrailRelationId => ({
-//     type: REMOVE_LIST_TRAIL_RELATION, 
-//     listTrailRelationId
-// })
+export const REMOVE_LIST_TRAIL_RELATION = "REMOVE_LIST_TRAIL_RELATION";
+const removeListTrailRelation = listTrailRelationId => ({
+    type: REMOVE_LIST_TRAIL_RELATION, 
+    listTrailRelationId
+})
 
-// // -------------------------------------------------
+// -------------------------------------------------
 
-// export const fetchListTrailRelation = listTrailRelationId => dispatch => (
-//     ListTrailApiUtils.fetchListTrailRelation(listTrailRelationId)
-//         .then(listTrailRelation => dispatch(receiveListTrailRelation(listTrailRelation)))
-// )
+export const fetchListTrailRelation = listTrailRelationId => dispatch => (
+    ListTrailApiUtils.fetchListTrailRelation(listTrailRelationId)
+        .then(listTrailRelation => dispatch(receiveListTrailRelation(listTrailRelation)))
+)
 
-// export const fetchListTrailRelations = listId => dispatch => (
-//     ListTrailApiUtils.fetchListTrailRelations(listId)
-//         .then(listTrailRelations => dispatch(receiveListTrailRelations(listTrailRelations)))
-// )
+export const fetchListTrailRelations = listId => dispatch => (
+    ListTrailApiUtils.fetchListTrailRelations(listId)
+        .then(listTrailRelations => dispatch(receiveListTrailRelations(listTrailRelations)))
+)
 
-// export const createListTrailRelation = (listId, trailId) => dispatch => (
-//     ListTrailApiUtils.createListTrailRelation(listId, trailId) 
-//         .then(listTrailRelation => dispatch(receiveListTrailRelation(listTrailRelation)))
-// )
+export const createListTrailRelation = (listId, trailId) => dispatch => (
+    ListTrailApiUtils.createListTrailRelation(listId, trailId) 
+        .then(listTrailRelation => dispatch(receiveListTrailRelation(listTrailRelation)))
+)
 
-// export const destroyListTrailRelation = listTrailRelationId => dispatch => (
-//     ListTrailApiUtils.destroyListTrailRelation(listTrailRelationId)
-//         .then(listTrailRelation => dispatch(removeListTrailRelation(listTrailRelation.id)))
-// )
+export const destroyListTrailRelation = listTrailRelationId => dispatch => (
+    ListTrailApiUtils.destroyListTrailRelation(listTrailRelationId)
+        .then(listTrailRelation => dispatch(removeListTrailRelation(listTrailRelation.id)))
+)
--- frontend/reducers/lists_trails_reducer.js
+++ frontend/reducers/lists_trails_reducer.js
@@ -1,19 +1,19 @@
-// import { RECEIVE_LIST_TRAIL_RELATION, RECEIVE_LIST_TRAIL_RELATIONS, REMOVE_LIST_TRAIL_RELATION } from "../actions/list_trail_actions";
+import { RECEIVE_LIST_TRAIL_RELATION, RECEIVE_LIST_TRAIL_RELATIONS, REMOVE_LIST_TRAIL_RELATION } from "../actions/list_trail_actions";
 
-// const ListsTrailsReducer = (oldState={}, action) => {
-//     Object.freeze(oldState);
-//     switch(action.type) {
-//         case RECEIVE_LIST_TRAIL_RELATIONS:
-//             return action.listTrailRelations; 
-//         case RECEIVE_LIST_TRAIL_RELATION:
-//             return Object.assign({}, oldState, { [action.listTrailRelation.id]: action.listTrailRelation });
-//         case REMOVE_LIST_TRAIL_RELATION:
-//             let newState = Object.assign({}, oldState);
-//             delete newState[action.listTrailRelationId];
-//             return newState;
-//         default:
-//             return oldState;
-//     }
-// }
+const ListsTrailsReducer = (oldState={}, action) => {
+    Object.freeze(oldState);
+    switch(action.type) {
+        case RECEIVE_LIST_TRAIL_RELATIONS:
+            return action.listTrailRelations; 
+        case RECEIVE_LIST_TRAIL_RELATION:
+            return Object.assign({}, oldState, { [action.listTrailRelation.id]: action.listTrailRelation });
+        case REMOVE_LIST_TRAIL_RELATION:
+            let newState = Object.assign({}, oldState);
+            delete newState[action.listTrailRelationId];
+            return newState;
+        default:
+            return oldState;
+    }
+}
 
-// export default ListsTrailsReducer;
+export default ListsTrailsReducer;
